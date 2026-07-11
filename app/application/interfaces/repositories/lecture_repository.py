ad590a172882ef2cb1790381f2377c6d5da75eb3
Sunnet <berdinov@gmail.com>
2026-07-12 01:19:54 +0300

    1.10 create_lecture implemented

--- app/application/interfaces/repositories/lecture_repository.py
+++ app/application/interfaces/repositories/lecture_repository.py
@@ -13,3 +13,7 @@ class LectureRepository(ABC):
     @abstractmethod
     async def get_by_ids(self, lecture_ids: list[UUID]) -> list[Lecture]:
         raise NotImplementedError
+    
+    @abstractmethod
+    async def add(self, lecture: Lecture) -> None:
+        raise NotImplementedError
