81a45548489f94fad4c8b08f5aabc3e3683ddaf9
Sunnet <berdinov@gmail.com>
2026-08-28 21:12:38 +0300

    2.2 updated code in user domain entity

--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -42,8 +42,23 @@ class User:
     def can_manage_learning_content(self) -> bool:
         return self.role in {UserRole.ADMIN, UserRole.AUTHOR}
 
+    def can_manage_course_structure(self) -> bool:
+        return self.role in {UserRole.ADMIN, UserRole.AUTHOR}
+    
+    def can_manage_interactive_content(self) -> bool:
+        return self.role in {UserRole.ADMIN, UserRole.AUTHOR}
+    
     def can_take_learning_activities(self) -> bool:
         return self.is_student()
+    
+    def can_view_own_learning_results(self) -> bool:
+        return self.is_student()
+    
+    def can_view_all_learning_results(self) -> bool:
+        return self.role in {UserRole.ADMIN, UserRole.AUTHOR}
 
     def can_manage_content(self) -> bool:
-        return self.can_manage_platform()
+        return self.can_manage_learning_content()
+    
+    
+
